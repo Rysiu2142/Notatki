90d5443cc7ce3a4e0dbd009b35f594ef45281d4a
Rysiu2142 <45450164+Rysiu2142@users.noreply.github.com>
2020-05-12 15:22:36 +0200

    more NoTES

--- BD/sql/db_schema.sql
+++ BD/sql/db_schema.sql
@@ -26,7 +26,7 @@ CREATE SEQUENCE "OcenyFilmu_SEQ" MINVALUE 1 MAXVALUE 9999 INCREMENT BY 1 START w
 
 --DLL for Oceny
 CREATE TABLE "Oceny" (
-  "idocena" NUMBER(*,0) constraint Ocenypk PRIMARY KEY,
+  "idocena" NUMBER(*,0) constraint Oceny_pk PRIMARY KEY,
   "Ocena" NUMBER(*,0),
   "Krytyk" varchar(255 byte) DEFAULT 'nie',
   "idksiazka" NUMBER(*,0)
@@ -34,14 +34,14 @@ CREATE TABLE "Oceny" (
 
 --DLL for Ksiazki
 CREATE TABLE "Ksiazki" (
-  "idksiazka" NUMBER(*,0) constraint Ksiazkipk PRIMARY KEY,
+  "idksiazka" NUMBER(*,0) constraint Ksiazki_pk PRIMARY KEY,
   "Tytul" varchar(255 byte)
 ) TABLESPACE "USERS"
 ;
 
 --DLL for Jezyki
 CREATE TABLE "Jezyki" (
-  "idjezyk" NUMBER(*,0) constraint Jezykipk PRIMARY KEY,
+  "idjezyk" NUMBER(*,0) constraint Jezyki_pk PRIMARY KEY,
   "nazwa" varchar(255 byte),
   "data_wydania" date,
   "idksiazka" NUMBER(*,)
@@ -50,7 +50,7 @@ CREATE TABLE "Jezyki" (
 
 --DLL for Autorzy
 CREATE TABLE "Autorzy" (
-  "idautor" NUMBER(*,0) constraint Autorzypk PRIMARY KEY,
+  "idautor" NUMBER(*,0) constraint Autorzy_pk PRIMARY KEY,
   "Imie" varchar(255 byte),
   "Nazwisko" varchar(255 byte),
   "urodzony" date,
@@ -60,7 +60,7 @@ CREATE TABLE "Autorzy" (
 
 --DLL for KsiazkiAutorzy
 CREATE TABLE "KsiazkiAutorzy" (
-  "idKA" NUMBER(*,0) constraint KsiazkiAutorzypk PRIMARY KEY,
+  "idKA" NUMBER(*,0) constraint KsiazkiAutorzy_pk PRIMARY KEY,
   "idautor" NUMBER(*,),
   "idksiazka" NUMBER(*,)
 ) TABLESPACE "USERS"
@@ -68,14 +68,14 @@ CREATE TABLE "KsiazkiAutorzy" (
 
 --DLL for Gatunki
 CREATE TABLE "Gatunki" (
-  "idgatunek" NUMBER(*,0) constraint Gatunkipk PRIMARY KEY,
+  "idgatunek" NUMBER(*,0) constraint Gatunki_pk PRIMARY KEY,
   "Nazwa" varchar(255 byte)
 ) TABLESPACE "USERS"
 ;
 
 --DLL for KsiazkiGatunki
 CREATE TABLE "KsiazkiGautnki" (
-  "idKG" NUMBER(*,0) constraint KsiazkiGautnkipk PRIMARY KEY,
+  "idKG" NUMBER(*,0) constraint KsiazkiGautnki_pk PRIMARY KEY,
   "idgatunek" NUMBER(*,),
   "idksiazka" NUMBER(*,)
 ) TABLESPACE "USERS"
@@ -83,7 +83,7 @@ CREATE TABLE "KsiazkiGautnki" (
 
 --DLL for Filmy
 CREATE TABLE "Filmy" (
-  "idfilm" NUMBER(*,0) constraint Filmypk PRIMARY KEY,
+  "idfilm" NUMBER(*,0) constraint Filmy_pk PRIMARY KEY,
   "Tytul" varchar(255 byte),
   "idksiazka" NUMBER(*,)
 ) TABLESPACE "USERS"
@@ -91,7 +91,7 @@ CREATE TABLE "Filmy" (
 
 --DLL for OcenyFilmu
 CREATE TABLE "OcenyFilmu" (
-  "idocena" NUMBER(*,0) constraint OcenyFilmupk PRIMARY KEY,
+  "idocena" NUMBER(*,0) constraint OcenyFilmu_pk PRIMARY KEY,
   "Ocena" NUMBER(*,0),
   "Krytyk" varchar(255 byte),
   "idfilm" NUMBER(*,)
@@ -100,14 +100,14 @@ CREATE TABLE "OcenyFilmu" (
 
 --DLL for Kategoria
 CREATE TABLE "Kategoria" (
-  "idkategoria" NUMBER(*,0) constraint Kategoriapk PRIMARY KEY,
+  "idkategoria" NUMBER(*,0) constraint Kategoria_pk PRIMARY KEY,
   "Nazwa" varchar(255 byte)
 ) TABLESPACE "USERS"
 ;
 
 --DLL for FilmyKategoria
 CREATE TABLE "FilmyKategoria" (
-  "idFK" NUMBER(*,0) constraint FilmyKategoriapk PRIMARY KEY, 
+  "idFK" NUMBER(*,0) constraint FilmyKategoria_pk PRIMARY KEY, 
   "idfilm" NUMBER(*,),
   "idkategoria" NUMBER(*,)
 ) TABLESPACE "USERS"
@@ -215,3 +215,178 @@ CREATE OR REPLACE TRIGGER FilmyKategoria_on_insert
   END;
   
 -----------------------------------------------------------------
+-- DDL for Procedure CREATE (INSERT)
+-----------------------------------------------------------------
+
+CREATE OR REPLACE PROCEDURE "CREATE_OCENA"(  Ocena_in IN NUMBER, Krytyk_in IN VARCHAR, idksiazka_in IN NUMBER) AS
+BEGIN
+  if( ocena_in > 10 ) THEN
+        ocena_in=10;
+  end if;
+  if( ocena_in < 0 ) THEN
+        ocena_in=0;
+  end if; 
+	INSERT INTO OCENY (Ocena,
+	Krytyk,
+	idksiazka)
+	VALUES (Ocena_in,Krytyk_in,idksiazka_in);
+END CREATE_OCENA
+
+-----------------------------------------------------------------
+
+CREATE OR REPLACE PROCEDURE "CREATE_Gatunki"
+(
+ Nazwa_in IN VARCHAR
+
+) AS
+BEGIN
+	INSERT INTO Gatunki (Nazwa)
+	VALUES (Nazwa_in);
+END CREATE_Gatunki
+	
+  
+CREATE OR REPLACE PROCEDURE "CREATE_KG"
+(
+ idgatunek_in IN NUMBER
+, idksiazka_in IN NUMBER
+) AS
+BEGIN
+	INSERT INTO KsiazkiGatunki (idgatunek,idksiazka)
+	VALUES (idgatunek_in,idksiazka_in);
+END CREATE_KG
+
+
+-----------------------------------------------------------------
+
+CREATE OR REPLACE PROCEDURE "CREATE_Ksiazki"
+(
+ tytul_in IN VARCHAR
+) AS
+BEGIN
+	INSERT INTO Ksiazki (Tytul)
+	VALUES (tytul_in);
+END CREATE_Ksiazki
+
+-----------------------------------------------------------------
+
+CREATE OR REPLACE PROCEDURE "CREATE_Jezyki"
+(
+ nazwa_in IN VARCHAR,
+ data_wyd IN DATE,
+ idks IN NUMBER 
+) AS
+BEGIN
+	INSERT INTO Jezyki (nazwa,data_wydania,idksiazka)
+	VALUES (nazwa_in,data_wyd,idks);
+END CREATE_Jezyki
+
+-----------------------------------------------------------------
+
+CREATE OR REPLACE PROCEDURE "CREATE_Autorzy"
+(
+ imie_in IN VARCHAR,
+ nazwisko_in IN VARCHAR,
+ urodz IN date ,
+ zmarl IN date
+) AS
+BEGIN
+	INSERT INTO Autorzy (Imie,Nazwisko,urodzony,zmarly)
+	VALUES (imie_in,nazwsko_in,urodz,zmarl);
+END CREATE_Autorzy
+
+CREATE OR REPLACE PROCEDURE "CREATE_KA"
+(
+ idautor_in IN NUMBER
+, idksiazka_in IN NUMBER
+) AS
+BEGIN
+	INSERT INTO KsiazkiAutorzy (idautorzy,idksiazka)
+	VALUES (idgatunek_in,idksiazka_in);
+END CREATE_KG
+
+-----------------------------------------------------------------
+-- Fimy i pochodne
+-----------------------------------------------------------------
+
+CREATE OR REPLACE PROCEDURE "CREATE_Filmy"
+(
+ tytul_in IN VARCHAR
+ ksiazka_in IN VARCHAR
+) AS
+BEGIN
+	INSERT INTO Filmy (Tytul,idksiazka)
+	VALUES (tytul_in,ksiazka_in);
+END CREATE_Ksiazki
+
+
+-----------------------------------------------------------------
+
+CREATE OR REPLACE PROCEDURE "CREATE_OcenyFilmu"
+(
+ ocena_in IN NUMBER
+ krytyk IN VARCHAR
+ idfilmu IN NUMBER
+) AS
+BEGIN
+  if( ocena_in > 10 ) THEN
+        ocena_in=10;
+  end if;
+  if( ocena_in < 0 ) THEN
+        ocena_in=0;
+  end if; 
+	INSERT INTO OcenyFilmu (Ocena,Krytyk,idfilm)
+	VALUES (tytul_in,ksiazka_in);
+END CREATE_OcenyFilmu
+
+-----------------------------------------------------------------
+
+CREATE OR REPLACE PROCEDURE "CREATE_Kategoria"
+(
+ Nazwa_in IN VARCHAR
+) AS
+BEGIN 
+	INSERT INTO Kategoria (Nazwa)
+	VALUES (Nazwa_in);
+END CREATE_Kategoria
+
+CREATE OR REPLACE PROCEDURE "CREATE_FK"
+(
+ idfilm_in IN NUMBER
+, idkategoria_in IN NUMBER
+) AS
+BEGIN
+	INSERT INTO FilmyKategoria (idfilm,idkategoria)
+	VALUES (idfilm_in,idkategoria_in);
+END CREATE_KG
+
+
+ALTER TABLE Oceny ADD FOREIGN KEY (idksiazka) REFERENCES Ksiazki (idksiazka);
+
+ALTER TABLE KsiazkiAutorzy ADD FOREIGN KEY (idautor) REFERENCES Autorzy (idautor);
+
+ALTER TABLE KsiazkiAutorzy ADD FOREIGN KEY (idksiazka) REFERENCES Ksiazki (idksiazka);
+
+ALTER TABLE KsiazkiGautnki ADD FOREIGN KEY (idgatunek) REFERENCES Gatunki (idgatunek);
+
+ALTER TABLE KsiazkiGautnki ADD FOREIGN KEY (idksiazka) REFERENCES Ksiazki (idksiazka);
+
+ALTER TABLE FilmyKategoria ADD FOREIGN KEY (idfilm) REFERENCES Filmy (idfilm);
+
+ALTER TABLE FilmyKategoria ADD FOREIGN KEY (idkategoria) REFERENCES Kategoria (idkategoria);
+
+ALTER TABLE Jezyki ADD FOREIGN KEY (idksiazka) REFERENCES Ksiazki (idksiazka);
+
+ALTER TABLE OcenyFilmu ADD FOREIGN KEY (idocena) REFERENCES Filmy (idfilm);
+
+ALTER TABLE Filmy ADD FOREIGN KEY (idfilm) REFERENCES Ksiazki (idksiazka);
+
+
+
+
+
+
+
+
+
+
+	
