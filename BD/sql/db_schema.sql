ee9f667e70264cb361a1a4ced646d731fd928dd9
Rysiu2142 <45450164+Rysiu2142@users.noreply.github.com>
2020-05-13 17:39:15 +0200

    staff

--- BD/sql/db_schema.sql
+++ BD/sql/db_schema.sql
@@ -1,362 +1,294 @@
 --DLL's for Sequences
-CREATE SEQUENCE "Oceny_SEQ" MINVALUE 1 MAXVALUE 9999 INCREMENT BY 1 START with 1 CACHE 20 ORDER NOCYCLE;
+CREATE SEQUENCE Oceny_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
 
-CREATE SEQUENCE "Ksiazki_SEQ" MINVALUE 1 MAXVALUE 9999 INCREMENT BY 1 START with 1 CACHE 20 ORDER NOCYCLE;
+CREATE SEQUENCE Ksiazki_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
 
-CREATE SEQUENCE "Jezyki_SEQ" MINVALUE 1 MAXVALUE 9999 INCREMENT BY 1 START with 1 CACHE 20 ORDER NOCYCLE;
+CREATE SEQUENCE Jezyki_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
 
 ------------------------------------------------------------------------------------------------
 -- DLL's for Sequence Autorzy-arm
-CREATE SEQUENCE "Autorzy_SEQ" MINVALUE 1 MAXVALUE 9999 INCREMENT BY 1 START with 1 CACHE 20 ORDER NOCYCLE;
-CREATE SEQUENCE "KsiazkiAutorzy_SEQ" MINVALUE 1 MAXVALUE 9999 INCREMENT BY 1 START with 1 CACHE 20 ORDER NOCYCLE;
+CREATE SEQUENCE Autorzy_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
+CREATE SEQUENCE KsiazkiAutorzy_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
 
 ------------------------------------------------------------------------------------------------
 -- DLL's for Sequence Gatunki-arm
-CREATE SEQUENCE "Gatunki_SEQ" MINVALUE 1 MAXVALUE 9999 INCREMENT BY 1 START with 1 CACHE 20 ORDER NOCYCLE;
-CREATE SEQUENCE "KsiazkiGatunki_SEQ" MINVALUE 1 MAXVALUE 9999 INCREMENT BY 1 START with 1 CACHE 20 ORDER NOCYCLE;
+CREATE SEQUENCE Gatunki_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
+CREATE SEQUENCE KsiazkiGatunki_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
 
 -------------------------------------------------------------------------------------------------
 -- DDL's for Sequence film-side
-CREATE SEQUENCE "Filmy_SEQ" MINVALUE 1 MAXVALUE 9999 INCREMENT BY 1 START with 1 CACHE 20 ORDER NOCYCLE;
-CREATE SEQUENCE "FilmyKategoria_SEQ" MINVALUE 1 MAXVALUE 9999 INCREMENT BY 1 START with 1 CACHE 20 ORDER NOCYCLE;
-CREATE SEQUENCE "Kategoria_SEQ" MINVALUE 1 MAXVALUE 9999 INCREMENT BY 1 START with 1 CACHE 20 ORDER NOCYCLE;
-CREATE SEQUENCE "OcenyFilmu_SEQ" MINVALUE 1 MAXVALUE 9999 INCREMENT BY 1 START with 1 CACHE 20 ORDER NOCYCLE;
+CREATE SEQUENCE Filmy_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
+CREATE SEQUENCE FilmyKategoria_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
+CREATE SEQUENCE Kategoria_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
+CREATE SEQUENCE OcenyFilmu_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1  NOCACHE NOCYCLE;
 
 --------------------------------------------------------------------------------------------------
 
 --DLL for Oceny
-CREATE TABLE "Oceny" (
-  "idocena" NUMBER(*,0) constraint Oceny_pk PRIMARY KEY,
-  "Ocena" NUMBER(*,0),
-  "Krytyk" varchar(255 byte) DEFAULT 'nie',
-  "idksiazka" NUMBER(*,0)
-) TABLESPACE "USERS" ;
+CREATE TABLE Oceny (
+  idocena NUMBER(10) PRIMARY KEY,
+  Ocena NUMBER(10),
+  Krytyk varchar2(255) DEFAULT 'nie',
+  idksiazka NUMBER(10)
+);
+
+
 
 --DLL for Ksiazki
-CREATE TABLE "Ksiazki" (
-  "idksiazka" NUMBER(*,0) constraint Ksiazki_pk PRIMARY KEY,
-  "Tytul" varchar(255 byte)
-) TABLESPACE "USERS"
-;
+CREATE TABLE Ksiazki (
+  idksiazka NUMBER(10) PRIMARY KEY,
+  Tytul varchar2(255)
+);
 
 --DLL for Jezyki
-CREATE TABLE "Jezyki" (
-  "idjezyk" NUMBER(*,0) constraint Jezyki_pk PRIMARY KEY,
-  "nazwa" varchar(255 byte),
-  "data_wydania" date,
-  "idksiazka" NUMBER(*,)
-) TABLESPACE "USERS"
-;
+CREATE TABLE Jezyki (
+  idjezyk NUMBER(10) PRIMARY KEY,
+  nazwa varchar2(255),
+  data_wydania date,
+  idksiazka NUMBER(10)
+);
 
 --DLL for Autorzy
-CREATE TABLE "Autorzy" (
-  "idautor" NUMBER(*,0) constraint Autorzy_pk PRIMARY KEY,
-  "Imie" varchar(255 byte),
-  "Nazwisko" varchar(255 byte),
-  "urodzony" date,
-  "zmarly" date
-) TABLESPACE "USERS"
-;
+CREATE TABLE Autorzy (
+  idautor NUMBER(10) PRIMARY KEY,
+  Imie varchar2(255),
+  Nazwisko varchar2(255),
+  urodzony date,
+  zmarly date
+);
 
 --DLL for KsiazkiAutorzy
-CREATE TABLE "KsiazkiAutorzy" (
-  "idKA" NUMBER(*,0) constraint KsiazkiAutorzy_pk PRIMARY KEY,
-  "idautor" NUMBER(*,),
-  "idksiazka" NUMBER(*,)
-) TABLESPACE "USERS"
-;
+CREATE TABLE KsiazkiAutorzy (
+  idKA NUMBER(10) PRIMARY KEY,
+  idautor NUMBER(10),
+  idksiazka NUMBER(10)
+);
 
 --DLL for Gatunki
-CREATE TABLE "Gatunki" (
-  "idgatunek" NUMBER(*,0) constraint Gatunki_pk PRIMARY KEY,
-  "Nazwa" varchar(255 byte)
-) TABLESPACE "USERS"
-;
+CREATE TABLE Gatunki (
+  idgatunek NUMBER(10) PRIMARY KEY,
+  Nazwa varchar2(255)
+);
 
 --DLL for KsiazkiGatunki
-CREATE TABLE "KsiazkiGautnki" (
-  "idKG" NUMBER(*,0) constraint KsiazkiGautnki_pk PRIMARY KEY,
-  "idgatunek" NUMBER(*,),
-  "idksiazka" NUMBER(*,)
-) TABLESPACE "USERS"
-;
+CREATE TABLE KsiazkiGatunki (
+  idKG NUMBER(10) PRIMARY KEY,
+  idgatunek NUMBER(10),
+  idksiazka NUMBER(10)
+);
 
 --DLL for Filmy
-CREATE TABLE "Filmy" (
-  "idfilm" NUMBER(*,0) constraint Filmy_pk PRIMARY KEY,
-  "Tytul" varchar(255 byte),
-  "idksiazka" NUMBER(*,)
-) TABLESPACE "USERS"
-;
+CREATE TABLE Filmy (
+  idfilm NUMBER(10) PRIMARY KEY,
+  Tytul varchar2(255),
+  idksiazka NUMBER(10)
+);
 
 --DLL for OcenyFilmu
-CREATE TABLE "OcenyFilmu" (
-  "idocena" NUMBER(*,0) constraint OcenyFilmu_pk PRIMARY KEY,
-  "Ocena" NUMBER(*,0),
-  "Krytyk" varchar(255 byte),
-  "idfilm" NUMBER(*,)
-) TABLESPACE "USERS"
-;
+CREATE TABLE OcenyFilmu (
+  idocena NUMBER(10) PRIMARY KEY,
+  Ocena NUMBER(10),
+  Krytyk varchar2(255) DEFAULT 'nie',
+  idfilm NUMBER(10)
+);
 
 --DLL for Kategoria
-CREATE TABLE "Kategoria" (
-  "idkategoria" NUMBER(*,0) constraint Kategoria_pk PRIMARY KEY,
-  "Nazwa" varchar(255 byte)
-) TABLESPACE "USERS"
-;
+CREATE TABLE Kategoria (
+  idkategoria NUMBER(10) PRIMARY KEY,
+  Nazwa varchar2(255)
+);
 
 --DLL for FilmyKategoria
-CREATE TABLE "FilmyKategoria" (
-  "idFK" NUMBER(*,0) constraint FilmyKategoria_pk PRIMARY KEY, 
-  "idfilm" NUMBER(*,),
-  "idkategoria" NUMBER(*,)
-) TABLESPACE "USERS"
-;
---------------------------------------------------------------------------
---DLL's for Triggers auto_increment on insert
---------------------------------------------------------------------------
-CREATE OR REPLACE TRIGGER Oceny_on_insert
-  Before Insert ON Oceny
-  FOR EACH ROW
-  BEGIN
-    SELECT Oceny_SEQ.nextval
-    INTO :new.idocena
-    FROM dual
-  END;
+CREATE TABLE FilmyKategoria (
+  idFK NUMBER(10) PRIMARY KEY, 
+  idfilm NUMBER(10),
+  idkategoria NUMBER(10)
+);
 
-CREATE OR REPLACE TRIGGER Ksiazki_on_insert
-  Before Insert ON Ksiazki
-  FOR EACH ROW
-  BEGIN
-    SELECT Ksiazki_SEQ.nextval
-    INTO :new.idksiazka
-    FROM dual
-  END;
-  
-CREATE OR REPLACE TRIGGER Jezyki_on_insert
-  Before Insert ON Jezyki
-  FOR EACH ROW
-  BEGIN
-    SELECT Jezyki_SEQ.nextval
-    INTO :new.idjezyk
-    FROM dual
-  END;
-  
-CREATE OR REPLACE TRIGGER Autorzy_on_insert
-  Before Insert ON Autorzy
-  FOR EACH ROW
-  BEGIN
-    SELECT Autorzy_SEQ.nextval
-    INTO :new.idautor
-    FROM dual
-  END;
-  
-CREATE OR REPLACE TRIGGER KsiazkiAutorzy_on_insert
-  Before Insert ON KsiazkiAutorzy
-  FOR EACH ROW
-  BEGIN
-    SELECT KsiazkiAutorzy_SEQ.nextval
-    INTO :new.idKA
-    FROM dual
-  END;
-  
-CREATE OR REPLACE TRIGGER Gatunki_on_insert
-  Before Insert ON Gatunki
-  FOR EACH ROW
-  BEGIN
-    SELECT Gatunki_SEQ.nextval
-    INTO :new.idgatunek
-    FROM dual
-  END;
-  
-CREATE OR REPLACE TRIGGER KsiazkiGautnki_on_insert
-  Before Insert ON KsiazkiGautnki
-  FOR EACH ROW
-  BEGIN
-    SELECT KsiazkiGautnki_SEQ.nextval
-    INTO :new.idKG
-    FROM dual
-  END;
-  
-CREATE OR REPLACE TRIGGER Filmy_on_insert
-  Before Insert ON Filmy
-  FOR EACH ROW
-  BEGIN
-    SELECT Filmy_SEQ.nextval
-    INTO :new.idfilm
-    FROM dual
-  END;
-  
-CREATE OR REPLACE TRIGGER OcenyFilmu_on_insert
-  Before Insert ON OcenyFilmu
-  FOR EACH ROW
-  BEGIN
-    SELECT OcenyFilmu_SEQ.nextval
-    INTO :new.idocena
-    FROM dual
-  END;
-  
-CREATE OR REPLACE TRIGGER Kategoria_on_insert
-  Before Insert ON Kategoria
-  FOR EACH ROW
-  BEGIN
-    SELECT Kategoria_SEQ.nextval
-    INTO :new.idkategoria
-    FROM dual
-  END;
-  
-CREATE OR REPLACE TRIGGER FilmyKategoria_on_insert
-  Before Insert ON FilmyKategoria
-  FOR EACH ROW
-  BEGIN
-    SELECT FilmyKategoria_SEQ.nextval
-    INTO :new.idFK
-    FROM dual
-  END;
-  
 -----------------------------------------------------------------
 -- DDL for Procedure CREATE (INSERT)
 -----------------------------------------------------------------
 
-CREATE OR REPLACE PROCEDURE "CREATE_OCENA"(  Ocena_in IN NUMBER, Krytyk_in IN VARCHAR, idksiazka_in IN NUMBER) AS
+CREATE OR REPLACE PROCEDURE CREATE_OCENA(
+  Ocena in OCENY.Ocena%TYPE,
+  Krytyk in OCENY.Krytyk%TYPE,
+  idksiazka in OCENY.idksiazka%TYPE
+  ) AS
 BEGIN
-  if( ocena_in > 10 ) THEN
-        ocena_in=10;
+  if( Ocena > 10 ) THEN
+        Ocena=10;
   end if;
-  if( ocena_in < 0 ) THEN
-        ocena_in=0;
+  if( Ocena < 0 ) THEN
+        Ocena=0;
   end if; 
-	INSERT INTO OCENY (Ocena,
-	Krytyk,
-	idksiazka)
-	VALUES (Ocena_in,Krytyk_in,idksiazka_in);
+	INSERT INTO OCENY 
+	VALUES 
+	(
+	OCENY_SEQ.nextval,
+	Ocena,Krytyk,
+	idksiazka
+	);
 END CREATE_OCENA
 
 -----------------------------------------------------------------
 
-CREATE OR REPLACE PROCEDURE "CREATE_Gatunki"
-(
- Nazwa_in IN VARCHAR
 
+
+
+CREATE OR REPLACE PROCEDURE CREATE_Gatunki
+(
+ Nazwa in GATUNKI.Nazwa%TYPE
 ) AS
 BEGIN
-	INSERT INTO Gatunki (Nazwa)
-	VALUES (Nazwa_in);
+	INSERT INTO Gatunki
+	VALUES (
+	Gatunki_SEQ.nextval,
+	Nazwa);
 END CREATE_Gatunki
 	
   
-CREATE OR REPLACE PROCEDURE "CREATE_KG"
+CREATE OR REPLACE PROCEDURE CREATE_KG
 (
- idgatunek_in IN NUMBER
-, idksiazka_in IN NUMBER
+ idgatunek in KsiazkiGatunki.idgatunek%TYPE
+, idksiazka in KsiazkiGatunki.idksiazka%TYPE
 ) AS
 BEGIN
-	INSERT INTO KsiazkiGatunki (idgatunek,idksiazka)
-	VALUES (idgatunek_in,idksiazka_in);
+	INSERT INTO KsiazkiGatunki
+	VALUES (
+	KsiazkiGatunki_SEQ.nextval,
+	idgatunek_in,
+	idksiazka_in);
 END CREATE_KG
 
 
 -----------------------------------------------------------------
 
-CREATE OR REPLACE PROCEDURE "CREATE_Ksiazki"
+CREATE OR REPLACE PROCEDURE CREATE_Ksiazki
 (
- tytul_in IN VARCHAR
+ tytul_in in Ksiazki.Tytul%TYPE
 ) AS
 BEGIN
-	INSERT INTO Ksiazki (Tytul)
-	VALUES (tytul_in);
+	INSERT INTO Ksiazki 
+	VALUES (
+	Ksiazki_SEQ.nextval,
+	tytul_in);
 END CREATE_Ksiazki
 
 -----------------------------------------------------------------
 
-CREATE OR REPLACE PROCEDURE "CREATE_Jezyki"
+CREATE OR REPLACE PROCEDURE CREATE_Jezyki
 (
- nazwa_in IN VARCHAR,
- data_wyd IN DATE,
- idks IN NUMBER 
+ nazwa in  Jezyki.nazwa%TYPE,
+ data_wyd in Jezyki.data_wydania%TYPE,
+ idks in Jezyki.idksiazka%TYPE 
 ) AS
 BEGIN
-	INSERT INTO Jezyki (nazwa,data_wydania,idksiazka)
-	VALUES (nazwa_in,data_wyd,idks);
+	INSERT INTO Jezyki 
+	VALUES (
+	Jezyki_SEQ.nextval,
+	nazwa_in,
+	data_wyd,
+	idks);
 END CREATE_Jezyki
 
 -----------------------------------------------------------------
 
-CREATE OR REPLACE PROCEDURE "CREATE_Autorzy"
+CREATE OR REPLACE PROCEDURE CREATE_Autorzy
 (
- imie_in IN VARCHAR,
- nazwisko_in IN VARCHAR,
- urodz IN date ,
- zmarl IN date
+ imie IN Autorzy.Imie%TYPE,
+ nazwisko IN Autorzy.Nazwisko%TYPE,
+ urodz IN Autorzy.urodzony%TYPE,
+ zmarl IN Autorzy.zmarly%TYPE
 ) AS
 BEGIN
-	INSERT INTO Autorzy (Imie,Nazwisko,urodzony,zmarly)
-	VALUES (imie_in,nazwsko_in,urodz,zmarl);
+	INSERT INTO Autorzy (
+	VALUES (
+	Autorzy_SEQ.nextval,
+	imie,
+	nazwsko,
+	urodz,
+	zmarl);
 END CREATE_Autorzy
 
-CREATE OR REPLACE PROCEDURE "CREATE_KA"
+CREATE OR REPLACE PROCEDURE CREATE_KA
 (
- idautor_in IN NUMBER
-, idksiazka_in IN NUMBER
+ idautor IN KsiazkiAutorzy.idautor%TYPE,
+ idksiazka IN KsiazkiAutorzy.idksiazka%TYPE
 ) AS
 BEGIN
-	INSERT INTO KsiazkiAutorzy (idautorzy,idksiazka)
-	VALUES (idgatunek_in,idksiazka_in);
-END CREATE_KG
+	INSERT INTO KsiazkiAutorzy
+	VALUES (
+	KsiazkiAutorzy_SEQ.nextval,
+	idautor,
+	idksiazka);
+END CREATE_KA
 
 -----------------------------------------------------------------
 -- Fimy i pochodne
 -----------------------------------------------------------------
 
-CREATE OR REPLACE PROCEDURE "CREATE_Filmy"
+CREATE OR REPLACE PROCEDURE CREATE_Filmy
 (
- tytul_in IN VARCHAR
- ksiazka_in IN VARCHAR
+ tytul IN Filmy.Tytul%TYPE,
+ idksiazka IN Filmy.idksiazka%TYPE
 ) AS
 BEGIN
-	INSERT INTO Filmy (Tytul,idksiazka)
-	VALUES (tytul_in,ksiazka_in);
-END CREATE_Ksiazki
+	INSERT INTO Filmy 
+	VALUES (
+	Filmy_SEQ.nextval,
+	tytul,
+	ksiazka);
+END CREATE_Filmy
 
 
 -----------------------------------------------------------------
 
-CREATE OR REPLACE PROCEDURE "CREATE_OcenyFilmu"
+CREATE OR REPLACE PROCEDURE CREATE_OcenyFilmu
 (
- ocena_in IN NUMBER
- krytyk IN VARCHAR
- idfilmu IN NUMBER
+ ocena IN OcenyFilmu.Ocena%TYPE
+ krytyk IN OcenyFilmu.Krytyk%TYPE
+ idfilmu IN OcenyFilmu.idfilm%TYPE
 ) AS
 BEGIN
-  if( ocena_in > 10 ) THEN
-        ocena_in=10;
+  if( ocena > 10 ) THEN
+        ocena=10;
   end if;
-  if( ocena_in < 0 ) THEN
-        ocena_in=0;
+  if( ocena < 0 ) THEN
+        ocena=0;
   end if; 
-	INSERT INTO OcenyFilmu (Ocena,Krytyk,idfilm)
-	VALUES (tytul_in,ksiazka_in);
+	INSERT INTO OcenyFilmu 
+	VALUES (
+	OcenyFilmu_SEQ.nextval,
+	ocena,
+	krytyk,
+	idfilmu
+	);
 END CREATE_OcenyFilmu
 
 -----------------------------------------------------------------
 
-CREATE OR REPLACE PROCEDURE "CREATE_Kategoria"
+CREATE OR REPLACE PROCEDURE CREATE_Kategoria
 (
- Nazwa_in IN VARCHAR
+ Nazwa IN Kategoria.Nazwa%TYPE
 ) AS
 BEGIN 
-	INSERT INTO Kategoria (Nazwa)
-	VALUES (Nazwa_in);
+	INSERT INTO Kategoria
+	VALUES (
+	Kategoria_SEQ.nextval,
+	Nazwa_in);
 END CREATE_Kategoria
 
-CREATE OR REPLACE PROCEDURE "CREATE_FK"
+CREATE OR REPLACE PROCEDURE CREATE_FK
 (
- idfilm_in IN NUMBER
-, idkategoria_in IN NUMBER
+ idfilm IN FilmyKategoria.idfilm%TYPE,
+ idkategoria IN FilmyKategoria.idkategoria%TYPE
 ) AS
 BEGIN
-	INSERT INTO FilmyKategoria (idfilm,idkategoria)
-	VALUES (idfilm_in,idkategoria_in);
+	INSERT INTO FilmyKategoria
+	VALUES (
+	FilmyKategoria_SEQ.nextval,
+	idfilm,
+	idkategoria);
 END CREATE_KG
 
 
@@ -366,9 +298,9 @@ ALTER TABLE KsiazkiAutorzy ADD FOREIGN KEY (idautor) REFERENCES Autorzy (idautor
 
 ALTER TABLE KsiazkiAutorzy ADD FOREIGN KEY (idksiazka) REFERENCES Ksiazki (idksiazka);
 
-ALTER TABLE KsiazkiGautnki ADD FOREIGN KEY (idgatunek) REFERENCES Gatunki (idgatunek);
+ALTER TABLE KsiazkiGatunki ADD FOREIGN KEY (idgatunek) REFERENCES Gatunki (idgatunek);
 
-ALTER TABLE KsiazkiGautnki ADD FOREIGN KEY (idksiazka) REFERENCES Ksiazki (idksiazka);
+ALTER TABLE KsiazkiGatunki ADD FOREIGN KEY (idksiazka) REFERENCES Ksiazki (idksiazka);
 
 ALTER TABLE FilmyKategoria ADD FOREIGN KEY (idfilm) REFERENCES Filmy (idfilm);
 
